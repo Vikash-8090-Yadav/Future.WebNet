4bde3531139eb46e36343e02fc0ce9897a666789
vikash yadav <yadav621gzp@gmail.com>
2022-06-15 00:49:48 +0530

    added

---
+++ Level0/Mood/Tru/node_modules/@truffle/hdwallet-provider/dist/index.d.ts
@@ -0,0 +1,24 @@
+import ProviderEngine from "web3-provider-engine";
+import type { JSONRPCRequestPayload, JSONRPCResponsePayload } from "ethereum-protocol";
+import type { ConstructorArguments } from "./constructor/ConstructorArguments";
+declare class HDWalletProvider {
+    private hdwallet?;
+    private walletHdpath;
+    private wallets;
+    private addresses;
+    private chainId?;
+    private chainSettings;
+    private hardfork;
+    private initialized;
+    engine: ProviderEngine;
+    constructor(...args: ConstructorArguments);
+    private initialize;
+    private checkBIP39Mnemonic;
+    private ethUtilValidation;
+    send(payload: JSONRPCRequestPayload, callback: (error: null | Error, response: JSONRPCResponsePayload) => void): void;
+    sendAsync(payload: JSONRPCRequestPayload, callback: (error: null | Error, response: JSONRPCResponsePayload) => void): void;
+    getAddress(idx?: number): string;
+    getAddresses(): string[];
+    static isValidProvider(provider: any): boolean;
+}
+export = HDWalletProvider;
