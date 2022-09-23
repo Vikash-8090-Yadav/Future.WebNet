71c35f9b77e4a9515c353045344fca1688d6ca68
Anmol <95535448+AnmolSirola@users.noreply.github.com>
2022-09-23 21:37:04 +0530

    Update SellNFT.js

--- Level2/NFT-MARKET-PLACE/src/components/SellNFT.js
+++ Level2/NFT-MARKET-PLACE/src/components/SellNFT.js
@@ -1,4 +1,4 @@
-import Navbar from "./Navbar";
+blueimport Navbar from "./Navbar";
 import { useState } from "react";
 import { uploadFileToIPFS, uploadJSONToIPFS } from "../pinata";
 import Marketplace from '../Marketplace.json';
@@ -93,19 +93,19 @@ export default function SellNFT () {
             <form className="bg-white shadow-md rounded px-8 pt-4 pb-8 mb-4">
             <h3 className="text-center font-bold text-purple-500 mb-8">Upload your NFT to the marketplace</h3>
                 <div className="mb-4">
-                    <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="name">NFT Name</label>
+                    <label className="block text-blue-500 text-sm font-bold mb-2" htmlFor="name">NFT Name</label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="Axie#4563" onChange={e => updateFormParams({...formParams, name: e.target.value})} value={formParams.name}></input>
                 </div>
                 <div className="mb-6">
-                    <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="description">NFT Description</label>
+                    <label className="block text-blue-500 text-sm font-bold mb-2" htmlFor="description">NFT Description</label>
                     <textarea className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" cols="40" rows="5" id="description" type="text" placeholder="Axie Infinity Collection" value={formParams.description} onChange={e => updateFormParams({...formParams, description: e.target.value})}></textarea>
                 </div>
                 <div className="mb-6">
-                    <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="price">Price (in ETH)</label>
+                    <label className="block text-blue-500 text-sm font-bold mb-2" htmlFor="price">Price (in ETH)</label>
                     <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="number" placeholder="Min 0.01 ETH" step="0.01" value={formParams.price} onChange={e => updateFormParams({...formParams, price: e.target.value})}></input>
                 </div>
                 <div>
-                    <label className="block text-purple-500 text-sm font-bold mb-2" htmlFor="image">Upload Image</label>
+                    <label className="block text-green-500 text-sm font-bold mb-2" htmlFor="image">Upload Image</label>
                     <input type={"file"} onChange={OnChangeFile}></input>
                 </div>
                 <br></br>
