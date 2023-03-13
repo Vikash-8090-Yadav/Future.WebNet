d363fc5952627e4a931de7224e398ec6aebc4556
SyedHussainSaber <syedhussainsaber8@gmail.com>
2023-03-13 17:03:48 +0530

    updated

--- Level2/NFT_Marketplace_New/FrontendUsingThegraph/frontend/pages/index.js
+++ Level2/NFT_Marketplace_New/FrontendUsingThegraph/frontend/pages/index.js
@@ -25,7 +25,7 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <main className={styles.main}>
-                <h1 className="font-bold py-4 px-4 text-2xl"> Recently Listed </h1>
+                <h1 className="font-bold py-4 px-4 text-2xl mx-8"> Recently Listed </h1>
                 <div className="flex flex-wrap">
                     <div className={styles.description}></div>
                     {isWeb3Enabled ? (
