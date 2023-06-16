5c6aac98ed9051d8fac25f4d1f42a621699839f0
Ayan Joshi <ayanjoshi182003@gmail.com>
2023-06-16 11:54:07 +0530

    commit

--- Frontend-Projects/BMI Calculator/script.js
+++ Frontend-Projects/BMI Calculator/script.js
@@ -1,46 +1,50 @@
-// Attach an event listener to the 'sub' element when clicked
-document.getElementById('sub').addEventListener('click', function() {
-    // Get the height and weight values from the input fields
-    let h = document.getElementById('height').value;
-    let w = document.getElementById('weight').value;
+document.addEventListener("DOMContentLoaded", function() {
+    // Attach an event listener to the 'sub' element when clicked
+    document.getElementById('sub').addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent form submission
   
-    // Convert height to meters
-    h /= 100.0;
+      // Get the height and weight values from the input fields
+      let h = document.getElementById('height').value;
+      let w = document.getElementById('weight').value;
   
-    // Calculate BMI
-    let bmi = w / (h * h);
-    
-    // Round BMI to two decimal places
-    bmi = parseFloat(bmi).toFixed(2);
+      // Convert height to meters
+      h /= 100.0;
   
-    let img;
-    let data = '';
+      // Calculate BMI
+      let bmi = w / (h * h);
   
-    // Determine the BMI category and corresponding image
-    if (bmi < 19) {
-      data = 'You Are Underweight';
-      img = "./Images/underweight.jpg";
-    } else if (bmi >= 19 && bmi <= 25) {
-      data = 'You Are Healthy';
-      img = "./Images/healthy.jpg";
-    } else if (bmi > 25 && bmi <= 30) {
-      data = 'You Are Overweight';
-      img = "./Images/overweight.jpg";
-    } else if (bmi > 30) {
-      data = 'You Are Obese';
-      img = "./Images/obese.jpg";
-    } else {
-      data = 'Please Enter a Valid Input';
-    }
+      // Round BMI to two decimal places
+      bmi = parseFloat(bmi).toFixed(2);
   
-    // Set the source attribute of the 'body' element to display the appropriate image
-    document.getElementById('body').setAttribute("src", img);
+      let img;
+      let data = '';
   
-    // Display the result and BMI information
-    document.getElementById('res').innerHTML = ` ${data}.`;
-    document.getElementById('result').innerHTML = `Your BMI is <strong>${bmi}</strong>.`;
+      // Determine the BMI category and corresponding image
+      if (bmi < 19) {
+        data = 'You Are Underweight';
+        img = "./Images/underweight.jpg";
+      } else if (bmi >= 19 && bmi <= 25) {
+        data = 'You Are Healthy';
+        img = "./Images/healthy.jpg";
+      } else if (bmi > 25 && bmi <= 30) {
+        data = 'You Are Overweight';
+        img = "./Images/overweight.jpg";
+      } else if (bmi > 30) {
+        data = 'You Are Obese';
+        img = "./Images/obese.jpg";
+      } else {
+        data = 'Please Enter a Valid Input';
+      }
   
-    // Show the 'info' card
-    document.getElementById('info').setAttribute("class", "card show");
+      // Set the source attribute of the 'body' element to display the appropriate image
+      document.getElementById('body').setAttribute("src", img);
+  
+      // Display the result and BMI information
+      document.getElementById('res').innerHTML = ` ${data}.`;
+      document.getElementById('result').innerHTML = `Your BMI is <strong>${bmi}</strong>.`;
+  
+      // Show the 'info' card
+      document.getElementById('info').setAttribute("class", "card show");
+    });
   });
   
