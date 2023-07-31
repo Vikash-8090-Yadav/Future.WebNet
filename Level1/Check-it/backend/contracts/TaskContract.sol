c0755c7897a43a4fdead96b58f88c9171bdf4d39
Abhishek Shukla <88367072+AVI-SHUKLA1110@users.noreply.github.com>
2023-07-31 17:17:24 +0530

    Update TaskContract.sol (#502)
    ### Bound Check for array input.
    +In the getMyTasks function, there is no bounds checking when copying tasks into the temporary array. If the counter exceeds the temporary array's length, it could lead to an exception. We can add a check to ensure counter doesn't exceed temporary's length.
    
    > So , I updated the file with required changes.

--- Level1/Check-it/backend/contracts/TaskContract.sol
+++ Level1/Check-it/backend/contracts/TaskContract.sol
@@ -24,22 +24,31 @@ external{
 
   }
 
-  function getMyTasks() external view returns(Task[] memory) {
-    Task[] memory temporary= new Task[](tasks.length);
-    uint counter=0;
+  function getMyTasks() external view returns (Task[] memory) {
+    Task[] memory temporary = new Task[](tasks.length);
+    uint counter = 0;
 
-    for (uint i=0; i<tasks.length; i++) {
-      if (taskToOwner[i]== msg.sender && tasks[i].isDeleted==false) {
-        temporary[counter]= tasks[i];
-        counter++;
-      }
+    for (uint i = 0; i < tasks.length; i++) {
+        if (taskToOwner[i] == msg.sender && !tasks[i].isDeleted) {
+            // Check if the counter is less than the temporary array's length
+            if (counter < temporary.length) {
+                temporary[counter] = tasks[i];
+            } else {
+                // If the counter exceeds the temporary array's length, exit the loop
+                break;
+            }
+            counter++;
+        }
     }
-    Task[] memory result= new Task[](counter);
-    for (uint i=0; i<counter; i++) {
-      result[i] = temporary[i];
+
+    // Create the result array with the actual size (counter)
+    Task[] memory result = new Task[](counter);
+    for (uint i = 0; i < counter; i++) {
+        result[i] = temporary[i];
     }
     return result;
-  }
+}
+
 
   function deleteTask(uint taskId, bool isDeleted) external {
     if (taskToOwner[taskId]== msg.sender) {
